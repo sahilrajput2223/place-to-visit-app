fdf44058f9b4da3d197b0cfd798d9497145749eb
Sahil Rajput <rajputsahil.2204@gmail.com>
2022-06-26 13:53:53 +0530

    Project Cleaning and Basic Code Added

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <React.Fragment>
+            <h1>
+                Place to visit
+            </h1>
+        </React.Fragment>
+    );
+}
+
+export default App
